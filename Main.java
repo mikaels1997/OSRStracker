ef09c8f4b51829877215f0e1d0b6208a1ba1bd13
jeretsu <thejeretsu@gmail.com>
2021-05-14 21:29:47 +0300

    Scrollbar to players

--- Main.java
+++ Main.java
@@ -5,6 +5,8 @@ import java.awt.GridLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 
 public class Main {
 
@@ -26,6 +28,15 @@ public class Main {
         new PlayerPanel("lol");
         new PlayerPanel("asd");
         new PlayerPanel("hehe");
+        new PlayerPanel("hehe2");
+        new PlayerPanel("hehe3");
+        new PlayerPanel("hehe4");
+        new PlayerPanel("hehe5");
+        new PlayerPanel("hehe6");
+        new PlayerPanel("hehe7");
+        new PlayerPanel("hehe8");
+        new PlayerPanel("hehe9");
+        new PlayerPanel("hehe10");
 
         mainFrame.setVisible(true);
     }
@@ -34,8 +45,10 @@ public class Main {
 
         sidePanel = new JPanel();
         sidePanel.setBackground(Color.red);
-        sidePanel.setLayout(new GridLayout(10, 1));
-        sidePanel.setVisible(true);
+        sidePanel.setLayout(new GridLayout(0, 1));
+        
+        JScrollPane scroll = new JScrollPane(sidePanel,ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        scroll.setVisible(true);
         
         new AddField(sidePanel);
 
@@ -43,9 +56,13 @@ public class Main {
         skillPanel.setBackground(Color.yellow);
         skillPanel.setPreferredSize(new Dimension(100,100));
         skillPanel.setLayout(new BorderLayout());
+        //JTextArea textarea = new JTextArea(100,100);
+        //JScrollPane scroll = new JScrollPane(textarea,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        //skillPanel.add(scroll);
         skillPanel.setVisible(true);
 
-        mainFrame.add(sidePanel, BorderLayout.WEST);
+        mainFrame.add(scroll, BorderLayout.WEST);
+        //mainFrame.add(sidePanel, BorderLayout.WEST);
         mainFrame.add(skillPanel, BorderLayout.CENTER);
     }
 }
